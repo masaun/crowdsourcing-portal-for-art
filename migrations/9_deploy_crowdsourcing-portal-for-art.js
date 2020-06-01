ff6dd0fb67c5e288928629b4ec1ab6e39d03c5e1
masaun <masanoriuno@gmail.com>
2020-06-01 09:35:04 +0900

    Fix migrateion-file of 9_deploy_crowdsourcing-portal-for-art.js

--- migrations/9_deploy_crowdsourcing-portal-for-art.js
+++ migrations/9_deploy_crowdsourcing-portal-for-art.js
@@ -1,12 +1,19 @@
 var DataBountyPlatform = artifacts.require("DataBountyPlatform");
 var IERC20 = artifacts.require("IERC20");
+var ILendingPool = artifacts.require("ILendingPool");
+var ILendingPoolCore = artifacts.require("ILendingPoolCore");
+var ILendingPoolAddressesProvider = artifacts.require("ILendingPoolAddressesProvider");
+
 
 //@dev - Import from exported file
 var contractAddressList = require('./contractAddress/contractAddress.js');
 var tokenAddressList = require('./tokenAddress/tokenAddress.js');
 var walletAddressList = require('./walletAddress/walletAddress.js');
 
-const daiAddress = tokenAddressList["Rinkeby"]["DAIaave"]; /// DAI address on Ropsten（from AAVE fancet）
+const daiAddress = tokenAddressList["Ropsten"]["DAIaave"]; /// DAI address on Ropsten（from AAVE fancet）
+const _lendingPool = contractAddressList["Ropsten"]["Aave"]["LendingPool"];
+const _lendingPoolCore = contractAddressList["Ropsten"]["Aave"]["LendingPoolCore"];
+const _lendingPoolAddressesProvider = contractAddressList["Ropsten"]["Aave"]["LendingPoolAddressesProvider"];
 
 const depositedAmount = web3.utils.toWei("0.15");    // 0.15 DAI which is deposited in deployed contract. 
 
@@ -14,7 +21,11 @@ module.exports = async function(deployer, network, accounts) {
     // Initialize owner address if you want to transfer ownership of contract to some other address
     let ownerAddress = walletAddressList["WalletAddress1"];
 
-    await deployer.deploy(DataBountyPlatform, daiAddress)
+    await deployer.deploy(DataBountyPlatform, 
+                          daiAddress,
+                          _lendingPool,
+                          _lendingPoolCore,
+                          _lendingPoolAddressesProvider)
                   .then(async function(dataBountyPlatform) {
                       if(ownerAddress && ownerAddress!="") {
                           console.log(`=== Transfering ownership to address ${ownerAddress} ===`)
